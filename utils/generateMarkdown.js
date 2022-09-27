5bcd607663cebb149605d611d94237247088a8d7
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-09-26 20:57:33 -0500

    fix spelling error

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,7 +13,7 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if( liscense === 'MIT') {
+  if( license === 'MIT') {
     return `## License
     MIT License
     Copyright (c)
@@ -22,7 +22,7 @@ function renderLicenseSection(license) {
     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
     `;
   } else {
-    if (liscense === 'Apache') {
+    if (license === 'Apache') {
       return `## License
       Apache License
       Version 2.0, January 2004
