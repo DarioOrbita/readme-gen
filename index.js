3042e4896cdcbad4b3f49a3b740a97e60a6514fb
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-09-26 20:10:45 -0500

    add question array

--- index.js
+++ index.js
@@ -6,7 +6,49 @@ const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
     {
-        
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of the project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe the project:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What steps are required to install the project?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Give instructions on how to use the project:'
+    },
+    {
+        type: 'list',
+        name: 'liscense',
+        choices: ['MIT', 'Apache', 'BSD']
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How can someone contribute to this project?'
+    },
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'How do you test this project?'
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Enter your GitHub username'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address'
     }
 ];
 
